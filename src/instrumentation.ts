bf7871155654e79d49d7bcf74dbb914ae5da19f1
justin <justin@ProGeniuss-MacBook-Air.local>
2026-09-02 11:37:20 +0800

    only backend

--- src/instrumentation.ts
+++
@@ -1,9 +0,0 @@
-export async function register() {
-  if (
-    process.env.NEXT_RUNTIME === "nodejs" &&
-    typeof globalThis.WebSocket === "undefined"
-  ) {
-    const { WebSocket } = await import("ws");
-    globalThis.WebSocket = WebSocket as typeof globalThis.WebSocket;
-  }
-}
